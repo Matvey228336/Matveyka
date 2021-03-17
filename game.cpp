c48d071a150b32b2dc4131da92fa4c4a52646bbe
Matvey228336 <71389247+Matvey228336@users.noreply.github.com>
2021-03-17 16:46:01 +0300

    Add files via upload

--- game.cpp
+++ game.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <conio.h>
 
 using namespace std;
 
@@ -12,8 +13,8 @@ eDirection dir;
 void Setup {
     gameOver = false;
     dir = STOP
-    x = width / 2;
-    y = height / 2;
+    x = width / 2 - 1;
+    y = height / 2 - 1;
     fruitX = rand () % width;
     fruitX = rand () % height;
     score = 0;
@@ -30,18 +31,67 @@ void Draw {
         for(int j = 0; j < width; j++) 
             if (j == 0 || j == width - 1)
                cout << "#";
+            if (i == y && j == x)
+               cout << "0";
+            else if (i == fruitY && j == fruitX)
+               cout << "F";
+            else
             cout << " ";
     }
         cout << endl;
     }
 
-    for (int i = 0; i < width, i++) 
+    for (int i = 0; i < width, i++) {
         cout << "#";
     cout << endl;
 }
+void Input {
+    if (_kbhit ()) {
+        switch (_getch())
+        {
+        case "a":
+        dir = Left;
+        break;
+        case "d":
+        dir = Right;
+        break;
+        case "w":
+        dir = Up;
+        break;
+        case "s":
+        dir = Down;
+        break;
+        case "x":
+        gameOver = true;
+        break;
+        }
+
+
+    }
+
+
+}
+
 
 void Logic {
+        switch (dir)
+        {
+        case Left:
+            x--;
+            break;
+        case Right:
+            x++;
+            break;
+        case Up:
+            y--;
+            break;
+        case Down:
+            y++;
+            break;
 
+
+
+        }
 
 
 }
